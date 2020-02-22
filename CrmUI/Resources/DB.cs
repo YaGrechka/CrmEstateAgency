1ba4fa7f336412033a6160856f6a97644764ad69
ValunEntertaiment <tim.valiev.00@mail.ru>
2020-02-22 12:13:40 +0500

    траблы с update

---
+++ CrmUI/Resources/DB.cs
@@ -0,0 +1,15 @@
+﻿namespace CrmUI.Resources
+{
+    class DB
+    {
+//        CREATE DATABASE Test;
+//--use Test
+//--CREATE TABLE Person(
+//--Id int NOT NULL Primary Key(Id),
+//--Name nchar (10) NULL,
+//--Phone nchar(10) NULL,
+//--Age nchar(10) NULL,
+//--Birthday nchar(10) NULL
+//--)
+    }
+}
